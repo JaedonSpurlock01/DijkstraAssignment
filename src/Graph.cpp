f62197dff2383ab6beec1ceb45e6cf190383d5d8
Cyphonvoid <yashaswi.kul@gmail.com>
2023-11-20 01:24:11 -0800

    Added new approaches and changes

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -9,7 +9,7 @@ class CityNode {
     
     private:
 
-    string _ID = ""; //ID, City_Code(2 letters), City_Name, Population, and Elevation.
+    string _ID = ""; 
     string _city_code = "";
     string _city_name = "";
     unsigned int _population = -1; 
@@ -18,24 +18,52 @@ class CityNode {
 
     public:
     
+    CityNode(string id, string code, string name, unsigned int ppl, int elevation){
+        this->_city_code = code; 
+        this->_city_name = name; 
+        this->_ID = id; 
+        this->_population = ppl; 
+        this->_elevation = elevation; 
+    }
+
 };
 
-
+/**
+ * [IMPORTANT]
+ * 
+ * 1) Sketch details of well thought OOP outline of the Graph data structure with every possible function
+ * 2) Break down code in functions in such a way that any team member can collaborate to complete this without needing to grasp full picture. 
+ * 3) Label each function and make commits for each function completed. 
+*/
 class CityGraph {
    
    private:
-   vector<CityNode> NodeList;
+   vector<CityNode*> NodeList;
    
    public:
-
-   void add_node(CityNode* node){
-
+    
+   //Commit G-0
+   void add_city(string id, string code, string name, unsigned int ppl, int elevation){
+      CityNode* city = new CityNode(id, code, name, ppl, elevation);
+      NodeList.push_back(city);
+   }
+   
+   //Commit 1
+   void delete_city(string id){
+      //Given a list, must delete a node using a id. 
+   }
+   
+   //Commit 2
+   void add_connection(){
+     //Establish connection using getters and setters between two nodes.
+   }
+   
+   //Commit 3
+   void delete_connection(){
+     //Given two nodes, erase the connection between them
+     //Access the info using nodes. 
    }
 
-   void delete_node(){
-
-   }
-
-
+   
 
 };

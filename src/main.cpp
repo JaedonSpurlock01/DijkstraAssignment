167321dc678226b3c9e1acd3dc085902d03c5075
JaedonSpurlock01 <112233517+JaedonSpurlock01@users.noreply.github.com>
2023-11-22 20:30:51 -0800

    Formatted main.cpp to look pretty

--- src/main.cpp
+++ src/main.cpp
@@ -5,41 +5,57 @@
 #include "error.cpp"
 #include <vector>
 
-const std::string CITY_FILE = "../lib/cities.txt";
-const std::string ROUTE_FILE = "../lib/routes.txt";
+const std::string CITY_FILE_PATH = "../lib/cities.txt";
+const std::string ROUTE_FILE_PATH = "../lib/routes.txt";
+const unsigned COMMAND_LINE_ARGS_COUNT = 3;
+
+void print_program_summary();
 
 int main(int argc, char **argv)
 {
-    // Verify correct number of command line arguments, terminate program if not 2
-    verify_commandline_args(argc, argv, 3);
+    /*------------------------------------------------------------------*/
+    // Verify and setup proper files
+    /*------------------------------------------------------------------*/
+    verify_commandline_args(argc, argv, COMMAND_LINE_ARGS_COUNT);
 
-    // Load city data
-    std::ifstream citiesFile(CITY_FILE);
-    std::ifstream routesFile(ROUTE_FILE);
+    std::ifstream citiesFile(CITY_FILE_PATH);
+    std::ifstream routesFile(ROUTE_FILE_PATH);
 
-    // Check if files are correct, if not terminate program
     verify_files({&citiesFile, &routesFile});
+    /*------------------------------------------------------------------*/
 
-    // Program summary
-    std::cout << "Authors: Jaedon & Yashaswi\nDate: 11/x/2023\nCourse: CS311 (Data Structures and Algorithms)\nDescription: Program to find the shortest route between cities\n";
-    std::cout << "-----------------------------------------------------\n";
+    print_program_summary();
 
-    // Create and setup city data in the form of a graph
+    /*------------------------------------------------------------------*/
+    // Initialize City Data
+    /*------------------------------------------------------------------*/
     CityGraph cities = CityGraph();
+
     std::vector<std::vector<std::string>> parsedCities = parse_cities_file(citiesFile);
     cities.add_multiple_cities(parsedCities);
+
     std::vector<std::vector<unsigned>> parsedRoutes = parse_routes_file(routesFile);
     cities.add_multiple_connections(parsedRoutes);
+    /*------------------------------------------------------------------*/
 
-    // Checking if the arguments exist or not in list of cities
+    /*------------------------------------------------------------------*/
+    // Verify if ID's exist, then print shortest path between them
+    /*------------------------------------------------------------------*/
     verify_city_ids([&cities](const std::string &cityID)
                     { return cities.search_by_city_id(cityID); },
                     {argv[1], argv[2]});
 
     cities.print_shortest_path_between(argv[1], argv[2]);
+    /*------------------------------------------------------------------*/
 
     // Program termination
     citiesFile.close();
     routesFile.close();
     return 0;
+}
+
+void print_program_summary()
+{
+    std::cout << "Authors: Jaedon & Yashaswi\nDate: 11/x/2023\nCourse: CS311 (Data Structures and Algorithms)\nDescription: Program to find the shortest route between cities\n";
+    std::cout << "-----------------------------------------------------\n";
 }
